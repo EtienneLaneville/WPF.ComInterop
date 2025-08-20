c81a6dcf2eb17f2cb4b5b68df4f09285b61c74a0
Etienne Laneville <etienne95@hotmail.com>
2025-08-20 12:10:02 -0700

    Removed fault Testbench project, created new one.

---
+++ Wpf.ComInterop.Testbench/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace Wpf.ComInterop.Testbench
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private SimpleComInteropClass? _simpleObject;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Initialize any resources or services needed for the application
+            // For example, you might want to set up a service locator or dependency injection container here
+
+            _simpleObject = new SimpleComInteropClass();
+            _simpleObject.ShowWindow();
+
+        }
+
+    }
+
+}
--- Wpf.ComInterop.Testbench/MainWindow.xaml.cs
+++ Wpf.ComInterop.Testbench/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Wpf.ComInteropTestbench
+namespace Wpf.ComInterop.Testbench
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
